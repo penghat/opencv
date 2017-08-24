56733d634a42dfbcf968807d66eabf7612a126c5
penghat <ananth.penghat@gmail.com>
2017-08-23 17:41:30 -0700

    Blur and threshold the images
    Done to remove noise and make identifying hand easier.
    Threshold not complete as it is not yet isolation hand/skin.

--- hand.py
+++ hand.py
@@ -3,15 +3,19 @@ import numpy as np
 
 cap = cv2.VideoCapture(0)
 
-while(True):
+while(True): # process individual frames of video
     ret, img = cap.read()
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    cv2.imshow('Frame', gray)
 
+    # apply transformations to make reading hand easier/cleaner
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # convert to gray
+    blurred = cv2.GaussianBlur(gray, (15, 15), 0) # apply gaussian blur
+    ret, thresh = cv2.threshold(blurred, 0, 255,  # apply threshold for B/W
+                                cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+
+    cv2.imshow('Frame', thresh)
     k = cv2.waitKey(30) & 0xFF
     if k == 27:
         break
 
 cap.release()
-
 cv2.destroyAllWindows()
