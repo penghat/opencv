c602db397571d42d2fdf6de6efce36bb37b67378
penghat <ananth.penghat@gmail.com>
2017-08-23 18:30:14 -0700

    Add region of interest
    Only manipulate this ROI area.
    Edit the threshold paramters a bit.

--- hand.py
+++ hand.py
@@ -6,13 +6,19 @@ cap = cv2.VideoCapture(0)
 while(True): # process individual frames of video
     ret, img = cap.read()
 
-    # apply transformations to make reading hand easier/cleaner
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # convert to gray
-    blurred = cv2.GaussianBlur(gray, (15, 15), 0) # apply gaussian blur
-    ret, thresh = cv2.threshold(blurred, 0, 255,  # apply threshold for B/W
-                                cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    # Indicate where to place hand and make that place a region of interest
+    cv2.rectangle(img, (100, 50), (550, 550), (255, 0, 0), 4)
+    img_roi = img[50:550, 100:550] # Crop out rectangle w/ hand
 
-    cv2.imshow('Frame', thresh)
+    # apply transformations to make reading hand easier/cleaner
+    gray = cv2.cvtColor(img_roi, cv2.COLOR_BGR2GRAY) # convert to gray
+    blurred = cv2.GaussianBlur(gray, (15, 15), 0) # apply gaussian blur
+    ret, thresh = cv2.threshold(blurred, 100, 255, # apply threshold for B/W
+                                cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+
+    cv2.imshow('ROI', img_roi)
+    cv2.imshow('After transformation', thresh)
+
     k = cv2.waitKey(30) & 0xFF
     if k == 27:
         break
