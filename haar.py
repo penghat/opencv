785653bb2387c34dce33c65b62c52d3e2dced018
penghat <ananth.penghat@gmail.com>
2017-09-05 22:14:06 -0400

    Edit so frame with boxes is shown

--- haar.py
+++ haar.py
@@ -7,15 +7,13 @@ cap = cv2.VideoCapture(0)
 
 while True:
     ret, img = cap.read()
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    hands = hand_cascade.detectMultiScale(gray)
-    print(len(hands))
+    hands = hand_cascade.detectMultiScale(gray, 7)
     for (x, y, w, h) in hands:
         cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
-    cv2.imshow('img', gray)
+    cv2.imshow('img', img)
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
